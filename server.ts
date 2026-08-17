283096069d12e30014ac570b9f7de6fc4f9a3d6e
aama012026 <aama012026@gmail.com>
2026-08-17 21:53:54 +0200

    Add: Flesh out makeCardTableRow in server.ts and accompanying css to serve rune icons and formatted badges (types, tags and keywords).

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import { SQL } from "bun"
-import { makeCardsTableBody, makeCardTableRow, makeCollectionPage, makeEnergySvg, makeMightSvg, makePowerSvg } from "./gen/HTMLtemplates"
+import { makeCardsTableBody, makeCardTableRow, makeCollectionPage, makeEnergySvg, makeMightSvg, makePowerSvg, makeTag } from "./gen/HTMLtemplates"
 import type { CardDetails, Cards } from "./gen/dbTableInterfaces"
 
 const sql = new SQL({
@@ -13,17 +13,31 @@ const sql = new SQL({
 })
 await sql`USE riftbound`
 const cards:Array<CardDetails> = await sql`SELECT * FROM card_details ORDER BY riot_id`
-const cardRows = cards.map(c => makeCardTableRow(
-	c.id, c.domains ?? 'null', c.rarity, c.set_code,
-	c.collector_number, Math.floor(Math.random() * 5), c.name,
-	c.energy ? makeEnergySvg(c.energy) : '',
-	c.power ? makePowerSvg(c.domains ?? 'rainbow').repeat(c.power) : '',
-	c.might ? makeMightSvg(c.might) : '',
-	c.types ?? '',
-	c.tags ?? '',
-	c.keywords ?? '',
-	c.description ?? ''
-))
+const cardRows = cards.map(c => {
+	let frame = ''
+	switch(c.rarity) {
+		case 'common':
+			frame='bronze'
+			break
+		case 'uncommon':
+			frame='silver'
+			break
+		default:
+			frame='gold'
+			break
+	}
+	return makeCardTableRow(
+		c.id, c.domains ?? 'null', c.rarity, c.set_code,
+		c.collector_number, Math.floor(Math.random() * 5), c.name,
+		c.energy ? makeEnergySvg(c.energy) : '',
+		c.power ? makePowerSvg(c.domains?.split(', ').join('-') + `-${frame}`).repeat(c.power) : '',
+		c.might ? makeMightSvg(c.might) : '',
+		c.types ?? '',
+		c.tags?.split(', ').map(makeTag).join('\n') ?? '',
+		c.keywords ?? '',
+		c.description ?? ''
+	)
+})
 
 const collection = makeCollectionPage(makeCardsTableBody(cardRows.join('\n')))
 
@@ -31,6 +45,15 @@ console.log(`Riftbound collection server version: 0`)
 const server = Bun.serve({
 	routes: {
 		'/': new Response(collection, {headers: {'Content-Type': 'text/html'}}),
+		'/fonts': (request) => {
+			const fontName = new URL(request.url).pathname
+			try {
+				return new Response(Bun.file(`.assets/fonts/${fontName}`))
+			}
+			catch {
+				return Response.json({message: `Could not find font: ${fontName}`})
+			}
+		},
 		'/*': (request) => {
 			try {
 				return new Response(Bun.file(`./assets/${new URL(request.url).pathname}`))
