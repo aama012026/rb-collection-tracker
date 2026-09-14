f640717cd434c10a0dbbc1ab310c1454d8d2cd50
aama012026 <aama012026@gmail.com>
2026-09-14 23:47:03 +0200

    Fix: sorting-signals and sort query. Add: filter-signals. TODO: filter query fragment.

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { SQL } from "bun"
-import { makeCardsTableBody, makeCollectionPage, makeCycleButton, makeFilterBar, makePopupMenu, makeRadioFilter} from "./gen/HTMLtemplates"
-import type { CardDetails, Cards, Domains, Keywords, Sets, Tags, Types } from "./gen/dbTableInterfaces"
+import { makeCardsTableBody, makeCollectionPage, makeCycleButton, makeFilterBar, makePopupMenu } from "./gen/HTMLtemplates"
+import type { CardDetails, Domains, Sets, Tags, Types } from "./gen/dbTableInterfaces"
 import { testLexer } from "./src/modules/test"
 import { testParser } from "./testParser"
 import { getCardTableRowHtml } from "./src/modules/rbmlHtmlRenderer"
@@ -17,45 +17,50 @@ const sql = new SQL({
 	bigint:true
 })
 await sql`USE riftbound`
-const sets:Pick<Sets, 'name'>[] = await sql`SELECT name FROM sets ORDER BY release_date`
-const domains:Pick<Domains, 'name'>[] = await sql`SELECT name FROM domains ORDER BY sort_order`
-const types:Pick<Types, 'name'>[] = await sql`SELECT name FROM types ORDER BY name`
-const tags:Pick<Tags, 'name'>[] = await sql`SELECT name FROM tags ORDER BY name`
+const sets:Pick<Sets, 'name'|'id'>[] = await sql`SELECT id, name FROM sets ORDER BY release_date`
+const domains:Pick<Domains, 'name'|'id'>[] = await sql`SELECT id, name FROM domains ORDER BY sort_order`
+const types:Pick<Types, 'name'|'id'>[] = await sql`SELECT id, name FROM types ORDER BY name`
+const tags:Pick<Tags, 'name'|'id'>[] = await sql`SELECT id, name FROM tags ORDER BY name`
 const cards:CardDetails[] = await sql`SELECT * FROM card_details ORDER BY riot_id`
 // Test
 testLexer(cards)
 testParser(cards)
 
 const cardRows = cards.map(c => getCardTableRowHtml(c))
-const setsFilters:Record<string, 'include'> = {}
-sets.forEach(set => setsFilters[set.name] = 'include')
-const domainsFilters:Record<string, 'include'> = {}
-domains.forEach(domain => domainsFilters[domain.name] = 'include')
-const typesFilters:Record<string, 'include'> = {}
-types.forEach(type => typesFilters[type.name] = 'include')
-const tagsFilters:Record<string, 'include'> = {}
-tags.forEach(tag => tagsFilters[tag.name] = 'include')
+
+const setsFilters:Record<number, 'include'> = {}
+const domainsFilters:Record<number, 'include'> = {}
+const typesFilters:Record<number, 'include'> = {}
+const tagsFilters:Record<number, 'include'> = {}
+
+sets.forEach(set => setsFilters[set.id] = 'include')
+domains.forEach(domain => domainsFilters[domain.id] = 'include')
+types.forEach(type => typesFilters[type.id] = 'include')
+tags.forEach(tag => tagsFilters[tag.id] = 'include')
 
 const collection = makeCollectionPage(
 	makeCardsTableBody(cardRows.join('\n')),
 	makeFilterBar(
-		stringify(setsFilters), stringify(domainsFilters), stringify(typesFilters), stringify(tagsFilters),
-		makePopupMenu('sets', sets.map(
-			s => makeCycleButton(`$setsFilters[${s.name}]`, s.name)).join('\n')
+		Bun.escapeHTML(stringify(setsFilters)),
+		Bun.escapeHTML(stringify(domainsFilters)),
+		Bun.escapeHTML(stringify(typesFilters)),
+		Bun.escapeHTML(stringify(tagsFilters)),
+		makePopupMenu('sets', sets.map(set =>
+			makeCycleButton(`$setsFilters[${set.id}]`, set.name)).join('\n')
 		),
-		makePopupMenu('domains', domains.map(
-			d => makeCycleButton(`$domainsFilters[${d.name}]`, d.name)).join('\n')
+		makePopupMenu('domains', domains.map(domain =>
+			makeCycleButton(`$domainsFilters[${domain.id}]`, domain.name)).join('\n')
 		),
-		makePopupMenu('types', types.map(
-			t => makeCycleButton(`$typesFilters[${t.name}]`, t.name)).join('\n')
+		makePopupMenu('types', types.map(type =>
+			makeCycleButton(`$typesFilters[${type.id}]`, type.name)).join('\n')
 		),
-		makePopupMenu('tags', tags.map(
-			t => makeCycleButton(`$tagsFilters[${t.name}]`, t.name)).join('\n')
+		makePopupMenu('tags', tags.map(tag =>
+			makeCycleButton(`$tagsFilters[${tag.id}]`, tag.name)).join('\n')
 		),
 	)
 )
 
-console.log(`Riftbound collection server version: 0`)
+console.log(`Riftbound collection server version: 0.7`)
 const server = Bun.serve({
 	routes: {
 		'/': new Response(collection, {headers: {'Content-Type': 'text/html; charset=utf-8',}}),
@@ -66,11 +71,15 @@ const server = Bun.serve({
 			}
 			const signals = JSON.parse(signalsString) as {sortOrder: string[], draggedIdx: number}
 			prettyPrint(signals)
-			const sortOrder = signals.sortOrder.join(', ')
-				.replace(/set/g, 'set_code')
-				.replace(/number/g, 'collector_number')
-				.replace(/domain/g, 'domains')
-				.replace(/type/g, 'types')
+			const sortOrder = signals.sortOrder.map(s => s
+				.replace('set', 'set_code')
+				.replace('number', 'collector_number')
+				.replace('domain', 'domains')
+				.replace('type', 'types')
+			)
+			// We escape each column name as a quoted identifier.
+			.map(column => sql(column))
+			.reduce((accumulated, column) => sql`${accumulated}, ${column}`)
 
 			prettyPrint(sortOrder)
 			const sortedCards:CardDetails[] = await sql`SELECT * FROM card_details ORDER BY ${sortOrder};`
